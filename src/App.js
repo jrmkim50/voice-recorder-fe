d2fee4dc6e1bbdd813be5438237f4b5cdbf34a52
jrmkim50 <jrmkim50@gmail.com>
2024-01-13 17:03:59 -0800

    audio recording works here

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import './App.css';
+import { AudioRecorder, useAudioRecorder } from 'react-audio-voice-recorder';
 
 function App() {
+  const recorderControls = useAudioRecorder(
+    {
+      noiseSuppression: true,
+      echoCancellation: true,
+    },
+    (err) => console.table(err) // onNotAllowedOrFound
+  );
+
+  const addAudioElement = (blob) => {
+    const url = URL.createObjectURL(blob);
+    const audio = document.createElement("audio");
+    audio.src = url;
+    audio.controls = true;
+    document.querySelector('.CommandBar').appendChild(audio);
+  };
+
   return (
     <div className="App">
-      
+      <div className='IntroBar'>
+        <h1>Deep Email Search</h1>
+        <h2>Here to give you clarity.</h2>
+        <p>Just speak into the microphone</p>
+      </div>
+      <div className='CommandBar'>
+        <h2>What do you need?</h2>
+        <AudioRecorder 
+          onRecordingComplete={(blob) => addAudioElement(blob)}
+          recorderControls={recorderControls}
+        />
+      </div>
     </div>
   );
 }
